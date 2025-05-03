29bd88c8fd14a1f07decbf72d9047f927b9796ed
earthmessenger <me@earthmessenger.xyz>
2025-05-03 16:44:10 +0800

    fix(web): addSpiderTasks uses Promise.allSettled but handles rejection improperly

--- packages/web-new/src/lib/query/spider.ts
+++ packages/web-new/src/lib/query/spider.ts
@@ -7,8 +7,12 @@ export async function addSpiderTask(uid: string | number) {
 }
 
 export async function addSpiderTasks(uids: (string | number)[]) {
-	const promises = Promise.allSettled(uids.map((uid) => fetch(`https://spider.benben.sbs/${uid}`)));
-	return promises
-		.then(() => addNotification('success', '成功'))
-		.catch(() => addNotification('error', '出了些问题。请稍候再试。'));
+	const results = await Promise.allSettled(uids.map((uid) => fetch(`https://spider.benben.sbs/${uid}`)));
+	const total = results.length;
+	const success = results.filter((r) => r.status === 'fulfilled').length;
+	if (success === total) {
+		addNotification('success', '成功');
+	} else {
+		addNotification('error', `出了些问题。请稍候再试。（成功率：${success}/${total}）`);
+	}
 }
