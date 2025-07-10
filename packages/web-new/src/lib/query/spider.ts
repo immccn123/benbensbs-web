4094591e4d16ea4e2e7cfd31d0a3691d3a5a3ef2
Imken Luo <me@imken.moe>
2025-07-11 00:08:20 +0800

    fix build error

--- packages/web-new/src/lib/query/spider.ts
+++ packages/web-new/src/lib/query/spider.ts
@@ -1,16 +1,20 @@
 import { addNotification } from '$lib/state/notifications';
 import { PUBLIC_SPIDER_BASE } from '$env/static/public';
 import { cleanupAllNotifications, subscribeTasks } from './ws';
+import { browser } from '$app/environment';
 
 export async function addSpiderTask(uid: string | number) {
-	subscribeTasks([+uid]);
+	if (browser) subscribeTasks([+uid]);
 	return await fetch(`${PUBLIC_SPIDER_BASE}/${uid}`)
 		.then(() => addNotification('success', '成功'))
-		.catch(() => {addNotification('error', '出了些问题。请稍候再试。'); cleanupAllNotifications();});
+		.catch(() => {
+			addNotification('error', '出了些问题。请稍候再试。');
+			cleanupAllNotifications();
+		});
 }
 
 export async function addSpiderTasks(uids: number[]) {
-	subscribeTasks(uids);
+	if (browser) subscribeTasks(uids);
 	const result = await fetch(`${PUBLIC_SPIDER_BASE}/${uids.join(',')}`);
 	if (result.ok) {
 		addNotification('success', '成功');
